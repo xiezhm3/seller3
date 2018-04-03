053f896c0d351ca101fd306f6adc24eaa2f98841
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2018-04-03 23:04:24 +0800

    remove unused file

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" @click="sayHi">
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  methods: {
-    sayHi () {
-      console.log('hi')
+  export default {
+    name: 'App',
+    methods: {
+    },
+    data () {
+      return {
+      };
     }
-  },
-  data () {
-    return {
-      msg: 123
-    }
-  }
-}
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
